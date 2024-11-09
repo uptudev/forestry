e321d3b925f88e1a6f4bd83592aa4953aa9c8a18
uptu <uptu@uptu.dev>
2024-11-09 13:28:38 -0800

    Front page doc update * `src/lib.rs`: Made the Rustdoc front page look better.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,21 @@
 //! A simple, efficient, concise, and elegant logging library for Rust.
 //!
-//! Provides a `Logger` struct that can be used to log messages to the console.
+//! Provides a [Logger](logs::Logger) struct that can be used to log messages to the console.
 //! The messages are coloured based on their severity level.
 //! Logs are output with a unique 16-bit log index.
 //! The log also contains a symbol to represent the severity level.
 //!
-//! All formatting can be configured using the `cfg()` method in conjunction with the
-//! `FormatOptions` enumerator. See the `logs` module for more details.
+//! Most of the time, you will only need to initialize a [Logger](logs::Logger) struct.
+//! After this,you can use the 
+//! - [info](logs::Logger::info), 
+//! - [warn](logs::Logger::warn),
+//! - [error](logs::Logger::error), 
+//! - [success](logs::Logger::success), and
+//! - [critical](logs::Logger::critical) methods to log messages.
+//!
+//! All formatting can be configured using the [cfg()](logs::Logger::cfg) method in conjunction
+//! with the [FormatOptions](logs::FormatOptions) enumerator.
+//! See the [logs] module for more details.
 
 pub mod logs;
 pub mod prelude {
